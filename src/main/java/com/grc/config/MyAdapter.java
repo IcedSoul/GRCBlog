489b948a5b0af4afc75b7d88073d15afbb32c6a2
icedsoul <1443700905@qq.com>
2017-06-30 14:47:59 +0800

    加了视图控制

--- src/main/java/com/grc/config/MyAdapter.java
+++ src/main/java/com/grc/config/MyAdapter.java
@@ -1,9 +1,11 @@
 package com.grc.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Created by 14437 on 2017/6/30.
@@ -14,7 +16,14 @@ public class MyAdapter extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers( ViewControllerRegistry registry ) {
         registry.addViewController( "/" ).setViewName( "forward:/login_register.html" );
+        registry.addViewController("/error").setViewName("error.html");
         registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
         super.addViewControllers( registry );
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        super.configurePathMatch(configurer);
+        configurer.setUseSuffixPatternMatch(false);
     }
 }
