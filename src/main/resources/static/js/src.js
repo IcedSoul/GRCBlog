f788565d93ccf21c4f296573b78c3079e47e4e37
陈雅莉 <yalichen19@163.com>
2017-09-09 16:08:17 +0800

    更新了图标

--- src/main/resources/static/js/src.js
+++ src/main/resources/static/js/src.js
@@ -1,4 +1,4 @@
-
+document.write('<link type="image/x-icon" rel="shortcut icon" href="img/favicon.ico" />');
 document.write('<link href="css/bootstrap.min.css" rel="stylesheet">');
 document.write('<link href="css/style.css" rel="stylesheet">');
 document.write('<script src="js/jquery.min.js"></script>');
